ab1f73e3a6cb1028404b08f35c36f0811d60f72c
Lauan Amorim <lauan12x@gmail.com>
2023-05-11 11:26:04 -0300

    11/05

---
+++ appLeitour/app/src/main/java/com/example/appleitour/livro.java
@@ -0,0 +1,14 @@
+package com.example.appleitour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class livro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livro);
+    }
+}
